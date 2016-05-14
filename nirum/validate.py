9159d5d79eb0557b90d8f03e28f872bbc548eaa2
Hyojun Kang <admire9@gmail.com>
2016-05-14 20:21:52 +0900

    validate_record_type

--- nirum/validate.py
+++ nirum/validate.py
@@ -2,7 +2,7 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~
 
 """
-__all__ = 'validate_boxed_type',
+__all__ = 'validate_boxed_type', 'validate_record_type',
 
 
 def validate_boxed_type(boxed, type_hint) -> bool:
@@ -10,3 +10,12 @@ def validate_boxed_type(boxed, type_hint) -> bool:
         raise TypeError('{0} expected, found: {1}'.format(type_hint,
                                                           type(boxed)))
     return True
+
+
+def validate_record_type(record) -> bool:
+    record_types = getattr(
+        record,
+        '_{0.__class__.__name__}__slot_types'.format(record)
+    )
+    return all(isinstance(getattr(record, attr), type_)
+               for attr, type_ in record_types.items())
--- tests/validate_test.py
+++ tests/validate_test.py
@@ -1,9 +1,13 @@
 from pytest import raises
 
-from nirum.validate import validate_boxed_type
+from nirum.validate import validate_boxed_type, validate_record_type
 
 
 def test_validate_boxed_type():
     assert validate_boxed_type(3.14, float)
     with raises(TypeError):
         validate_boxed_type('hello', float)
+
+
+def test_validate_record_type(fx_point):
+    assert validate_record_type(fx_point)
